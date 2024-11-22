f5d2131b7259b4e9aff036e69919fe520e792326
CloudBeesTechnology <CloudBeesTechnology@users.noreply.github.com>
2024-11-22 08:02:54 +0000

    Commit the artifacts.

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "adinin%20logo.png": "3442e93529e23f67392a1b20b74a93c5",
-"main.dart.js": "8b0f78599a800867a5698d259e772eac",
+"main.dart.js": "46c6b10aca99d079f33e57f4dc66a3e6",
 "assets/FontManifest.json": "866b9b20ab0e8c30ffe220d2a2d66abe",
 "assets/AssetManifest.bin": "4579192af421d53f0050402a73de5b4c",
 "assets/fonts/MaterialIcons-Regular.otf": "c26b70c38fba54832d2f2ed684711558",
@@ -57,7 +57,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "5e76247e7dfd8043a6e58c7708a3621f",
-"flutter_bootstrap.js": "9d2a306a486f64d7faaea9a7874f4b3c"};
+"flutter_bootstrap.js": "3f4802ba63ad3af34ae14f70a983c68a"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
